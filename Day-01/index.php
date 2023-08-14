c39bcc2720f74d261d9b8668f83772c9ed58bdac
Pankaj Chouhan <36913690+Pankaj-Str@users.noreply.github.com>
2023-08-14 19:06:27 +0530

    Add files via upload

---
+++ Day-01/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>p4n.in</p>
+
+    <?php 
+        echo "www.codeswithpankaj.com";
+        echo("<br><h3>p4n</h3>");
+        echo(800);
+        echo("<br>"."Welcome "."p4n");
+        echo("<br>".(34+90));
+        echo("<br>".(45<78));
+    ?>
+    <hr>
+    <?php
+    //variable - store data temp...
+    $name = "joy";
+    $age = 12;
+    $height = 3.4;
+    ?>
+
+    <ul>
+        <li><?php echo($name); ?></li>
+        <li><?php echo($age); ?></li>
+        <li><?php echo($height); ?></li>
+    </ul>
+
+</body>
+</html>
